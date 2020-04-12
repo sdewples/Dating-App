dd84c30af9030707e4661818d346fa4cec77687b
sdewples <56274860+sdewples@users.noreply.github.com>
2020-04-12 15:26:44 -0400

    More UI improvements and implemented Angular JWT

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,17 +12,20 @@ export class RegisterComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit(): void {}
 
   register() {
     this.authService.register(this.model).subscribe(
       () => {
-        console.log('registered');
+        this.alertify.success('registered');
       },
       (error) => {
-        console.log(error);
+        this.alertify.error(error);
       }
     );
   }
